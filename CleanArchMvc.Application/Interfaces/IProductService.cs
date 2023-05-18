88076d30e85b5053a47d37b3e4595fb8fbb0c5d8
Patricio Osterno Rios <patricio.osterno1@gmail.com>
2023-05-17 23:22:05 -0300

    Incluídas algumas classes ligadas a ao projeto Application: Service e DTOs, inserido o AutoMapper e iniciada a inclusão dos Controllers na WebUI

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProductsAsync();
+
+        Task<ProductDTO> GetByIdAsync(int? id);
+
+        Task<ProductDTO> GetProductCategoryAsync(int? id);
+
+        Task Add(ProductDTO productDTO);
+
+        Task Update(ProductDTO productDTO);
+
+        Task Remove(int? id);
+    }
+}
---
+++ CleanArchMvc.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetCategories();
+            return View(result);
+        }
+    }
+}
